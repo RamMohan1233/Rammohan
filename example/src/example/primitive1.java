4891f21a1f6939fb9d2d5918fb82af3c329c01eb
RamMohan1233 <rammohansimham@gmail.com>
2022-07-14 09:24:24 +0530

    test

---
+++ example/src/example/primitive1.java
@@ -0,0 +1,14 @@
+package example;
+
+public class primitive1 {
+	public static void main(String[] args)
+	{
+        byte a=Byte.MAX_VALUE;
+		byte b= Byte.MIN_VALUE;
+		System.out.println(a);
+		System.out.println(b);
+		 char[] c= {'r','a','m'};
+		 String d=new String (c);
+		 System.out.println(d);
+	}
+}
